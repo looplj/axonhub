aa3335734c06c794ba3fdb769c6432b1954b1d91
looplj <bababaa261@gmail.com>
2025-08-17 13:10:42 +0800

    feat: add close button to toast

--- frontend/src/components/ui/sonner.tsx
+++ frontend/src/components/ui/sonner.tsx
@@ -10,6 +10,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
     <Sonner
       theme={theme as ToasterProps["theme"]}
       className="toaster group pointer-events-auto"
+      closeButton
       style={
         {
           "--normal-bg": "var(--popover)",
