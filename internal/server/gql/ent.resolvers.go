8bd09fea947759e2a5f983cab860967330098f03
Matt Cowger <matt@cowger.us>
2026-01-25 17:29:49 -0800

    chore: clean up old commented GraphQL resolver code after regeneration

--- internal/server/gql/ent.resolvers.go
+++ internal/server/gql/ent.resolvers.go
@@ -865,20 +865,3 @@ type usageLogResolver struct{ *Resolver }
 type userResolver struct{ *Resolver }
 type userProjectResolver struct{ *Resolver }
 type userRoleResolver struct{ *Resolver }
-
-// !!! WARNING !!!
-// The code below was going to be deleted when updating resolvers. It has been copied here so you have
-// one last chance to move it out of harms way if you want. There are two reasons this happens:
-//  - When renaming or deleting a resolver the old code will be put in here. You can safely delete
-//    it when you're done.
-//  - You have helper methods in this file. Move them out to keep these resolver files clean.
-/*
-	func (r *channelResolver) ProviderQuotaStatus(ctx context.Context, obj *ent.Channel) (*ent.ProviderQuotaStatus, error) {
-	pqs, err := obj.ProviderQuotaStatus(ctx)
-	if ent.IsNotFound(err) {
-		return nil, nil
-	}
-
-	return pqs, err
-}
-*/
