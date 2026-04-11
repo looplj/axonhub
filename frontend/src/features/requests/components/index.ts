84e310e37adf43450305f1305a982a66cbe0a1dd
Loop <bababaa261@gmail.com>
2026-04-11 11:00:47 +0800

    fix: do not unescape json for request/reponse body, close #1357 (#1360)

--- frontend/src/features/requests/components/index.ts
+++ frontend/src/features/requests/components/index.ts
@@ -3,4 +3,3 @@ export { DataTableToolbar } from './data-table-toolbar';
 export { DataTableViewOptions } from './data-table-view-options';
 export { DataTablePagination } from './data-table-pagination';
 export { useRequestsColumns } from './requests-columns';
-export { JsonViewerDialog } from './json-viewer-dialog';
