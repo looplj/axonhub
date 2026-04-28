663c0ef68632ea61e8c1460459f1ffaa81a09f5d
djdembeck <dembeck.david@protonmail.com>
2026-04-27 23:12:17 -0500

    fix: use request-scoped context in goroutine (G118)

--- internal/server/biz/provider_quota.go
+++ internal/server/biz/provider_quota.go
@@ -309,7 +309,7 @@ func (svc *ProviderQuotaService) registerNeuralWattSupport() {
 }
 
 func (svc *ProviderQuotaService) Start(ctx context.Context) error {
-	go svc.loadQuotaCache(context.Background())
+	go svc.loadQuotaCache(ctx)
 
 	cronExpr := svc.intervalToCronExpr(svc.getCheckInterval())
 
