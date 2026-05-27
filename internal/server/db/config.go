a84624457a2667d9add46c97f1d14e9b0e33ce23
王煜鑫 <wxh657211334@163.com>
2026-05-27 14:39:44 +0800

    feat: support disabling db schema initialization

--- internal/server/db/config.go
+++ internal/server/db/config.go
@@ -11,6 +11,7 @@ type Config struct {
 	ConnMaxLifetime      time.Duration `conf:"conn_max_lifetime" yaml:"conn_max_lifetime" json:"conn_max_lifetime"`
 	ConnMaxIdleTime      time.Duration `conf:"conn_max_idle_time" yaml:"conn_max_idle_time" json:"conn_max_idle_time"`
 	DisableSQLiteAutoWAL bool          `conf:"disable_sqlite_auto_wal" yaml:"disable_sqlite_auto_wal" json:"disable_sqlite_auto_wal"`
+	DisableSchemaInit    bool          `conf:"disable_schema_init" yaml:"disable_schema_init" json:"disable_schema_init"`
 
 	ReadReplica ReadReplicaConfig `conf:"read_replica" yaml:"read_replica" json:"read_replica"`
 }
